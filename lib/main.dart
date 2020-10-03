2aa3c177002749425fbbba90cd9fd551e0c20b95
SPrio <mahantasupriyo@gmail.com>
2020-10-03 11:43:21 +0530

    added scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello, World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://static.next-episode.net/tv-shows-images/huge/the-flash.jpg'),
+          ),
+        ),
       ),
     ),
   );
